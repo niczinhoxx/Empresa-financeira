03e0459d0e2c8ad08cd31356749981fb39abcd15
niczinhoxx <nicolastiquinho007@gmail.com>
2026-03-03 20:41:27 -0300

    feat: criação de roles e definição de permissões de acesso

--- scripts/_roles_permissions.sql
+++ scripts/_roles_permissions.sql
@@ -2,13 +2,22 @@
 --acesso 
 create user dba_admin with password 'admin123'; 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dba_admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dba_admin;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO dba_admin;
 
--- Criação do log do analista com as permissões de select,insert, update
--- e delete em todas as tabelas
+
+
+-- Criação do log do analista com as permissões de select e insert
+-- na tabela de transacões
 create user analista with password 'analista123';
-grant select, insert, update, delete on all tables in schema to analista;
+grant select, insert  on transacoes to analista;
+
+
+
+
 
 -- Criação do log do usuario_consulta, com apenas a permissão de constultar 
--- todas as tabelas
+-- as viewa
 create user usuario_consulta with password 'usuario123';
-grant select on all tables in schema to usuario_consulta;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO usuario_consulta;
